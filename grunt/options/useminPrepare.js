8b5bb674fe19770df5b7b233b89a8934b48a66d4
Otho <marcelo.lopes.lotufo@gmail.com>
2016-02-20 14:49:04 -0200

    Remova bower e atualize dependencias do npm
    Vou tentar user só um gestor de dependencias ao invés de dois

--- grunt/options/useminPrepare.js
+++
@@ -1,9 +0,0 @@
-// Reads HTML for usemin blocks to enable smart builds that automatically
-// concat, minify and revision files. Creates configurations in memory so
-// additional tasks can operate on them
-module.exports = {
-    options: {
-        dest: '<%= pkg.dist %>'
-    },
-    html: '<%= pkg.app %>/index.html'
-};
