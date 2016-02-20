a67c7b833519e18056fcc3d1cacefd8dd6706f3e
Otho <marcelo.lopes.lotufo@gmail.com>
2016-02-20 12:39:34 -0200

    Atualize grunt e pasta de tarefas
    Foi mudada a estrutura da pasta para que fique mais organizada, as tarefas
    e configurações para o grunt ficam todas dentro da pasta grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,37 @@
-'use strict';
+"use strict";
 
-module.exports = function (grunt) {
+module.exports = function( grunt ) {
 
     // Time how long tasks take. Can help when optimizing build times
-    require('time-grunt')(grunt);
+    require("time-grunt")( grunt );
 
     var config = {
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
         env: process.env
     };
 
     // Automatically load required grunt tasks
-    // load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
-    require('load-grunt-tasks')(grunt);
-    grunt.loadTasks('tasks');
+    // load all grunt tasks matching the ["grunt-*", "@*/grunt-*"] patterns
+    require("load-grunt-tasks")( grunt );
+    grunt.loadTasks("tasks");
 
     // Function to load config separately
-    function loadConfig(path) {
-        var glob = require('glob');
-        var object = {};
-        var key;
+    function loadConfig( path ) {
+        var glob = require("glob"),
+            object = {},
+            key;
 
-        glob.sync('*', {cwd: path}).forEach(function(option) {
-            key = option.replace(/\.js$/, '');
-            object[key] = require(path + option);
+        glob.sync("*", { cwd: path }).forEach(function( option ) {
+            key = option.replace( /\.js$/, "" );
+            object[ key ] = require( path + option );
         });
 
         return object;
     }
 
     // Magical line that actually load tasks configs from path below
-    grunt.util._.extend(config, loadConfig('./tasks/options/'));
+    grunt.util._.extend( config, loadConfig("./grunt/options/") );
 
     // Inits configuration
-    grunt.initConfig(config);
-
-    // By default, your `index.html`'s <!-- Usemin block --> will take care
-    // of minification. These next options are pre-configured if you do not
-    // wish to use the Usemin blocks.
-    // cssmin: {
-    //   dist: {
-    //     files: {
-    //       '<%= config.dist %>/styles/main.css': [
-    //         '.tmp/styles/{,*/}*.css',
-    //         '<%= config.app %>/styles/{,*/}*.css'
-    //       ]
-    //     }
-    //   }
-    // },
-    // uglify: {
-    //   dist: {
-    //     files: {
-    //       '<%= config.dist %>/scripts/scripts.js': [
-    //         '<%= config.dist %>/scripts/scripts.js'
-    //       ]
-    //     }
-    //   }
-    // },
-    // concat: {
-    //   dist: {}
-    // },
+    grunt.initConfig( config );
 };
