e329bf48b58022043291f9fa2bd21984be3ba352
Otho <marcelo.lopes.lotufo@gmail.com>
2016-03-01 02:37:13 -0300

    Resuma arquivo Gruntfile
    Retire parte do código responsável por carregar arquivos de configuração e delegue essa função para o módulo load-grunt-config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,21 @@
 "use strict";
 
 module.exports = function( grunt ) {
+    var path = require("path");
 
     // Time how long tasks take. Can help when optimizing build times
     require("time-grunt")( grunt );
-
-    var config = {
-        pkg: grunt.file.readJSON("package.json"),
-        env: process.env
-    };
 
     // Automatically load required grunt tasks
     // load all grunt tasks matching the ["grunt-*", "@*/grunt-*"] patterns
     require("load-grunt-tasks")( grunt );
     grunt.loadTasks("./grunt/tasks/");
 
-    // Function to load config separately
-    function loadConfig( path ) {
-        var glob = require("glob"),
-            object = {},
-            key;
-
-        glob.sync("*", { cwd: path }).forEach(function( option ) {
-            key = option.replace( /\.js$/, "" );
-            object[ key ] = require( path + option );
-        });
-
-        return object;
-    }
-
-    // Magical line that actually load tasks configs from path below
-    grunt.util._.extend( config, loadConfig("./grunt/options/") );
-
     // Inits configuration
-    grunt.initConfig( config );
+    require("load-grunt-config")( grunt, {
+        configPath: path.join( process.cwd(), "grunt/options/"),
+        data: {
+            pkg: grunt.file.readJSON("package.json")
+        }
+    });
 };
